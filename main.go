c30ca7fd71538fc941a2e73f73838b2035ed070e
Ignat Drozdov <idrozdov@beringresearch.com>
2020-04-10 16:19:49 +0100

    bcl lexer and parser implementation

--- main.go
+++ main.go
@@ -2,36 +2,84 @@ package main
 
 import (
 	"fmt"
-	"github.com/beringresearch/bcl/parser"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/alecthomas/participle"
+	"github.com/alecthomas/repr"
 )
 
-// PackagesConf ...
-type PackagesConf struct {
-	Manager string
-	System  []string
+type Bool bool
+
+func (b *Bool) Capture(v []string) error { *b = v[0] == "true"; return nil }
+
+type Value struct {
+	Boolean    *Bool    `  @("true"|"false")`
+	Identifier *string  `| @Ident { @"." @Ident }`
+	String     *string  `| @(String|Char|RawString)`
+	Number     *float64 `| @(Float|Int)`
+	Array      []*Value `| "[" { @@ [ "," ] } "]"`
 }
 
-// RunConf ...
-type RunConf struct {
-	Command []string
+func (l *Value) GoString() string {
+	switch {
+	case l.Boolean != nil:
+		return fmt.Sprintf("%v", *l.Boolean)
+	case l.Identifier != nil:
+		return fmt.Sprintf("`%s`", *l.Identifier)
+	case l.String != nil:
+		return fmt.Sprintf("%q", *l.String)
+	case l.Number != nil:
+		return fmt.Sprintf("%v", *l.Number)
+	case l.Array != nil:
+		out := []string{}
+		for _, v := range l.Array {
+			out = append(out, v.GoString())
+		}
+		return fmt.Sprintf("[]*Value{ %s }", strings.Join(out, ", "))
+	}
+	panic("??")
 }
 
-// Environ ...
-type Environ struct {
-	Base     string
-	Packages PackagesConf
-	Run      RunConf
+type Entry struct {
+	Key   string `@Ident`
+	Value *Value `( ":" @@`
+	Block *Block `| @@ )`
+}
+
+type Block struct {
+	Parameters []*Value `{ @@ }`
+	Entries    []*Entry `"{" { @@ } "}"`
+}
+
+type Config struct {
+	Entries []*Entry `{ @@ }`
 }
 
 func main() {
-	conf := parser.New("Bravefile.bcl")
-	env := &Environ{}
-	err := conf.Unmarshal(env)
-	if err == nil {
-		fmt.Println(env)
-	} else {
-		fmt.Println(err)
+	if len(os.Args) < 2 {
+		panic("no valid file name or path provided for file!")
 	}
 
-	//fmt.Println(env.Run.Command[1])
+	path := os.Args[1]
+	absPath, _ := filepath.Abs(path)
+	file, err := ioutil.ReadFile(absPath)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	parser, err := participle.Build(&Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	expr := &Config{}
+	err = parser.ParseString(string(file), expr)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	repr.Println(expr)
 }
